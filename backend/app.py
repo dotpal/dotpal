a47d7cf1a81245135e466a4b5de368ca3341da84
ikeda042 <ikeda042main@gmail.com>
2024-02-18 22:30:48 +0900

    app

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,37 @@
-# main.py
 from fastapi import FastAPI
 import uvicorn
+from .schemas import User, Coordinate
+from datetime import datetime
+
+user_1 = {
+    "username": "user_1",
+    "email": "user1@user.email",
+    "original_id": "npm-rendezvous",
+    "coordinate": {"lat": 35.6895, "lon": 139.691},
+    "created_at": datetime.now(),
+}
+
+user_2 = {
+    "username": "user_2",
+    "email": "ericCartman@eric.us",
+    "original_id": "fatAss752",
+    "coordinate": {"lat": 35.6895, "lon": 139.695},
+    "created_at": datetime.now(),
+}
+
+user_3 = {
+    "username": "user_3",
+    "email": "cto@gmail.com",
+    "original_id": "Chris Chang",
+    "coordinate": {"lat": 35.6895, "lon": 139.6918},
+    "created_at": datetime.now(),
+}
 
 app = FastAPI(docs_url="/api/docs", redoc_url="/api/redoc")
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "from FastAPI!"}
 
 
