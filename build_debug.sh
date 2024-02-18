c84997b2e6e0a47d3c0c7e158c35905a79d835d7
ikeda042 <ikeda042main@gmail.com>
2024-02-18 18:52:13 +0900

    build_debug

---
+++ build_debug.sh
@@ -0,0 +1,4 @@
+printf '<!doctype html><body style=margin:0><link rel=stylesheet href=styles.css>' > index.html
+printf '<script>' >> index.html
+cat client.js >> index.html
+printf '</script>' >> index.html
