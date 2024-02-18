576422ed4859850e3675e06d76576fd3d04364c9
ikeda042 <ikeda042main@gmail.com>
2024-02-18 18:52:34 +0900

    build debug shell script

--- build_debug.sh
+++ build_debug.sh
@@ -1,4 +1,6 @@
+#build_debug.sh
 printf '<!doctype html><body style=margin:0><link rel=stylesheet href=styles.css>' > index.html
 printf '<script>' >> index.html
 cat client.js >> index.html
 printf '</script>' >> index.html
+node server.js
